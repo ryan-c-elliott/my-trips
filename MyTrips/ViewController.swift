e153778c84bebd305e528d330ad27ead407d425d
Ryan Elliott <rce3986@gmail.com>
2021-07-23 23:16:35 -0400

    Initial Commit

---
+++ MyTrips/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTrips
+//
+//  Created by Ryan Elliott on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
