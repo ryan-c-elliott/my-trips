725111efeb3547ff31516b03b7c73c5e54856851
Ryan Elliott <rce3986@gmail.com>
2021-07-25 16:24:16 -0400

    removed setMap function. moved code for button press to the didReceiveLocation function

--- MyTrips/ViewControllers/TripViewController.swift
+++ MyTrips/ViewControllers/TripViewController.swift
@@ -56,23 +56,17 @@ class TripViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    // Sets the map to a new location
-    func setMap(_ manager: CLLocationManager) {
-        self.map.centerCoordinate = manager.location!.coordinate
-    }
     
     /* * CLLocationManagerDelegate * */
     
     // When new location is retrieved
     func locationManager(_ manager: CLLocationManager,
                          didUpdateLocations locations: [CLLocation]) {
-        
-        setMap(manager)
-        
-        
-        
-        
-        
+        if self.tripButton.trip {   // Trip was just started
+            self.map.centerCoordinate = manager.location!.coordinate
+        } else {    // Trip was just ended
+            
+        }
         
     }
 
@@ -96,40 +90,42 @@ class TripViewController: UIViewController, CLLocationManagerDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func requestLocationServices() {
+        // present an alert indicating location authorization required
+        // and offer to take the user to Settings for the app via
+        // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error!", message: "Location services needs to be enabled to start a trip.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Settings", style: UIAlertAction.Style.default, handler: { (alert: UIAlertAction!) in
+                print("")
+                UIApplication.shared.open(NSURL(string: UIApplication.openSettingsURLString)! as URL)
+                
+                }
+            ))
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { (alert: UIAlertAction!) in
+                print("")
+
+                self.dismiss(animated: true, completion: nil)
+                }
+            ))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
 
     @IBAction func tripButtonTapped(_ sender: TripButton) {
         if !sender.location {
 
-            // present an alert indicating location authorization required
-            // and offer to take the user to Settings for the app via
-            // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
-            DispatchQueue.main.async {
-                let alert = UIAlertController(title: "Error!", message: "Location services needs to be enabled to start a trip.", preferredStyle: UIAlertController.Style.alert)
-                alert.addAction(UIAlertAction(title: "Settings", style: UIAlertAction.Style.default, handler: { (alert: UIAlertAction!) in
-                    print("")
-                    UIApplication.shared.openURL(NSURL(string:UIApplication.openSettingsURLString)! as URL)
-                    
-                    }
-                ))
-                alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { (alert: UIAlertAction!) in
-                    print("")
-
-                    self.dismiss(animated: true, completion: nil)
-                    }
-                ))
-                self.present(alert, animated: true, completion: nil)
-                
-            }
-            
+            requestLocationServices()
             return
         }
-        if sender.trip {    // Stop the trip
-            
-        } else {    // Start the trip
-            
-        }
-        self.manager.requestLocation()
+
+        
         sender.toggle()
+        self.manager.requestLocation()
+        
+        // Most work will be done in the locationManagerDidUpdateLocations function
         
         
     }
