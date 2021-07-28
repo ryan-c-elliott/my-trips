66032f5ea9438031957145539e8f0258d9d7c7a9
Ryan Elliott <rce3986@gmail.com>
2021-07-28 18:52:35 -0400

    added json helpers

--- MyTrips/Helpers.swift
+++ MyTrips/Helpers.swift
@@ -20,3 +20,31 @@ func mid(_ v1: Double, _ v2: Double) -> Double {
 func toCLLocation(_ loc: Location) -> CLLocation {
     CLLocation(latitude: loc.latitude, longitude: loc.longitude)
 }
+
+func loadJson(url: URL) -> ResponseData? {
+    
+    do {
+
+        let data = try Data(contentsOf: url)
+        
+        let responseData = try JSONDecoder().decode(ResponseData.self, from: data)
+        return responseData
+    } catch {
+        print("either the data didn't load or groups couldn't be retrieved")
+    }
+    
+    return nil
+}
+
+// URL for data in Application Storage
+func getURL(filename: String) -> URL? {
+    do {
+        let url = try FileManager.default
+            .url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent(filename)
+        return url
+    } catch {
+        print("url couldn't be retrieved")
+    }
+    return nil
+}
