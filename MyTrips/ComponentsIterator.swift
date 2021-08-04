293805dd54db2d0532ee45f99f2e17cd4c1a92af
Ryan Elliott <rce3986@gmail.com>
2021-08-03 21:08:54 -0400

    added nextIndex and next functions to iterator. added getters to iterator

--- MyTrips/ComponentsIterator.swift
+++ MyTrips/ComponentsIterator.swift
@@ -19,6 +19,7 @@ class ComponentsIterator: TripIterator {
     private var year: Int? = nil
     private var month: Int? = nil
     private var day: Int? = nil
+    private var trip: Int? = nil
     
     init(_ components: Components, start: Date, end: Date) {
         self.components = components
@@ -32,8 +33,53 @@ class ComponentsIterator: TripIterator {
         }
     }
     
+    private func getYear() -> Year {
+        self.components.years[self.year!]
+    }
+    
+    private func getMonth() -> Month {
+        getYear().months[self.month!]
+    }
+    
+    private func getDay() -> Day {
+        getMonth().days[self.day!]
+    }
+    
+    private func getTrip() -> Trip {
+        getDay().trips[self.trip!]
+    }
+    
+    private func nextIndex() -> (Int, Int, Int, Int)? {
+        if self.trip == nil {
+            return (self.year!, self.month!, self.day!, 0)
+        }
+        let day: Day = getDay()
+        if self.trip! + 1 < day.trips.count {
+            return (self.year!, self.month!, self.day!, self.trip! + 1)
+        }
+        let month: Month = getMonth()
+        if self.day! + 1 < month.days.count {
+            return (self.year!, self.month!, self.day! + 1, 0)
+        }
+        let year: Year = getYear()
+        if self.month! + 1 < year.months.count {
+            return (self.year!, self.month! + 1, 0, 0)
+        }
+        if self.year! + 1 < self.components.years.count {
+            return (self.year! + 1, 0, 0, 0)
+        }
+        return nil
+    }
+    
     func next() -> Trip? {
-        nil
+        if let (year, month, day, trip) = nextIndex() {
+            self.year = year
+            self.month = month
+            self.day = day
+            self.trip = trip
+            return getTrip()
+        }
+        return nil
     }
     
     
